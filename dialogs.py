01e9a6e4967c0b670f7799566246f5186b5a5008
user <behinder3@gmail.com>
2018-10-04 11:51:57 +0300

    20181004_11_51

---
+++ dialogs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+a = input("Ceiniijamaias lietotaaj, pozalustya, ievadi skaitliiti: ")
+a = int(a)
+print("Lietotaaj, tub esi iebadiijis skaitliiti:%d"%(a))
+aa = a * a
+print("Tavs skaitlis kvadrātā ir: %d"%(aa))
+
