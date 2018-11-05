0cf3da57833f094f119d502d6a8944dc462ee4c7
user <behinder3@gmail.com>
2018-11-05 15:35:01 +0200

    20181105_15_34

---
+++ cavbob.py
@@ -0,0 +1,83 @@
+'''
+str1= "sweiks " 
+str2= "tur"
+bob = str1 + str2
+print(bob)
+str3 = '123'
+x=int(str3) +1
+print(x)
+
+name = input('Enter: ')
+print(name)
+apple = input('Enter: ')
+x = int(apple)-10
+print(x)
+
+Vaards = 'Viktorija'
+letter = Vaards[1]
+print(letter)
+x = 3
+w = Vaards[x-1]
+print(w)
+
+viktorija = 'anastasija'
+print(len(viktorija))
+
+fruit = 'viktorija'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index +1
+
+word = 'viktorija'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count= count + 1
+        print(count)
+
+s = 'Viktorija 111'
+print(s[0:9])
+print(s[10:13])
+print(s[9:3])
+
+a = 'Viktorija'
+b = a + '111'
+print(b)
+c = a + ' ' + '111'
+print(c)
+
+fruit = 'Viktorija'
+'V' in fruit
+'o' in fruit
+'ktor' in fruit
+if 'i' in fruit :
+    print('nawol')
+
+greet= 'Hello Viktorija'
+zap = greet.lower()
+print(zap)
+print(greet)
+print('zdr Viktorija'.lower())
+
+fruit = 'Viktorija'
+pos = fruit.find('tor')
+print(pos)
+aa = fruit.find('ija')
+print(aa)
+
+greet = 'Viktorija zdr'
+nnn = greet.upper()
+print(nnn)
+www = greet.lower()
+print(www)
+
+greet = 'Gutten tag Viktorija'
+nstr = greet.replace('Viktorija','111')
+print(nstr)
+nstr = greet.replace('o','a')
+print(nstr)
+'''
+greet = ' sweika Viktorijas kundze '
+greet.lstrip()
